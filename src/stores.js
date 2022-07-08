519893d88905bd527f52b5e92a45fa9816fd1056
sookyeongyeom <sookyeong.dev@gmail.com>
2022-07-08 21:43:33 +0900

    :sparkles: authToken

--- src/stores.js
+++ src/stores.js
@@ -9,7 +9,56 @@ function setArticleContent() {}
 function setArticleMode() {}
 function setComments() {}
 function setAuth() {}
-function setAuthToken() {}
+
+function setAuthToken() {
+    const token = localStorage.getItem('authToken');
+    const { subscribe, set } = writable(token);
+
+    const login = async (email, password) => {
+        try {
+            const options = {
+                path: '/login',
+                data: {
+                    email,
+                    password,
+                },
+            };
+
+            const response = await postApi(options);
+            const token = response.authToken;
+
+            localStorage.setItem('authToken', token);
+            set(token);
+            router.goto('/articles');
+        } catch (error) {
+            alert('오류가 발생했습니다. 다시 시도해주세요.');
+        }
+    };
+
+    const logout = async () => {
+        try {
+            const options = {
+                path: '/logout',
+            };
+            await postApi(options);
+            authToken.resetAuthToken();
+        } catch (error) {
+            alert('오류가 발생했습니다. 다시 시도해주세요');
+        }
+    };
+
+    const resetAuthToken = async () => {
+        set('');
+        localStorage.removeItem('authToken');
+    };
+
+    return {
+        subscribe,
+        login,
+        logout,
+        resetAuthToken,
+    };
+}
 
 export const currentArticlesPage = setCurrentArticlesPage();
 export const articles = setArticles();
