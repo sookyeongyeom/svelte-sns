0cb1da8f7ce8d8234300ee86ec08fd1bf92e3db9
sookyeongyeom <sookyeong.dev@gmail.com>
2022-07-08 22:08:43 +0900

    :sparkles: check login & fetch userdata

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,15 @@
 <script>
     import Router from './router.svelte';
     import { auth, authToken } from './stores';
+
+    $: {
+        if ($authToken) {
+            // authToken의 변화 감지
+            auth.isLogin(); // authToken에서 변화 감지 후 토큰이 있을 때 실행
+        } else {
+            auth.resetUserInfo(); // authToken에서 변화 감지 후 토큰이 사라졌을 때 실행
+        }
+    }
 </script>
 
 <div class="mdl-layout__container">
