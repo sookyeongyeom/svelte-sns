47021c310d224b48ecf54751d0d3c3629a6f3570
sookyeongyeom <sookyeong.dev@gmail.com>
2022-07-08 21:53:29 +0900

    :sparkles: auth

--- src/stores.js
+++ src/stores.js
@@ -8,7 +8,58 @@ function setLoadingArticle() {}
 function setArticleContent() {}
 function setArticleMode() {}
 function setComments() {}
-function setAuth() {}
+
+function setAuth() {
+    let initValues = {
+        _id: '',
+        email: '',
+    };
+
+    let values = { ...initValues };
+
+    const { subscribe, set, update } = writable(values);
+
+    const isLogin = async () => {
+        try {
+            const getUserInfo = await getApi({ path: '/user' });
+            set(getUserInfo);
+        } catch (error) {
+            // 토큰이 비정상적이면 리셋
+            auth.resetUserInfo();
+            authToken.resetAuthToken();
+        }
+    };
+
+    const resetUserInfo = () => {
+        const newValues = { ...initValues };
+        set(newValues);
+    };
+
+    const register = async (email, password) => {
+        try {
+            const options = {
+                path: '/users',
+                data: {
+                    email,
+                    password,
+                },
+            };
+
+            await postApi(options);
+            alert('가입이 완료되었습니다.');
+            router.goto('/login');
+        } catch (error) {
+            alert('오류가 발생했습니다. 다시 시도해주세요.');
+        }
+    };
+
+    return {
+        subscribe,
+        isLogin,
+        resetUserInfo,
+        register,
+    };
+}
 
 function setAuthToken() {
     const token = localStorage.getItem('authToken');
