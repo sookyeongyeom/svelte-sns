a529ea8a325f341ac8be6dfcdb556c7404903311
sookyeongyeom <sookyeong.dev@gmail.com>
2022-07-09 14:48:57 +0900

    :sparkles: scroll skeleton

--- src/components/Article.svelte
+++ src/components/Article.svelte
@@ -1,32 +1,33 @@
+<script>
+    import { articlesMode } from '../stores';
+
+    export let article;
+</script>
+
 <!-- start article box-->
 <div class="box mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
     <div class="mdl-card mdl-cell mdl-cell--12-col">
         <div class="mdl-card__supporting-text ">
             <div class="info-box">
                 <div class="info">
-                    <p class="user-id">freeseamew</p>
-                    <p class="post-day">2021-11-11</p>
+                    <p class="user-id">{article.userName}</p>
+                    <p class="post-day">{article.createdAt}</p>
                 </div>
             </div>
         </div>
         <div class="mdl-card__supporting-text ">
             <p class="pre">
-                Dolore ex deserunt aute fugiat aute nulla ea sunt aliqua nisi
-                cupidatat eu. Nostrud in laboris labore nisi amet do dolor eu
-                fugiat consectetur elit cillum esse. Pariatur occaecat nisi
-                laboris tempor laboris eiusmod qui id Lorem esse commodo in.
-                Exercitation aute dolore deserunt culpa consequat elit labore
-                incididunt elit anim.
+                {article.content}
             </p>
         </div>
         <div class="mdl-card__actions mdl-card--border">
             <!-- <a href="#null" class="mdl-button">Read our features</a> -->
             <div class="icon-box">
                 <i class="bx bx-heart" />
-                <p>10</p>
+                <p>{article.likeCount}</p>
             </div>
             <div class="icon-box-comment">
-                <p>10</p>
+                <p>{article.commentCount}</p>
                 <i class="bx bx-comment" />
             </div>
         </div>
--- src/components/ArticleList.svelte
+++ src/components/ArticleList.svelte
@@ -1,3 +1,51 @@
+<script>
+    import { onMount } from 'svelte';
+    import { articles, currentArticlesPage } from '../stores';
+    import Article from './Article.svelte';
+
+    let component;
+    let elementScroll;
+
+    onMount(() => {
+        articles.resetArticles();
+        articles.fetchArticles();
+    });
+
+    $: {
+        if (component || elementScroll) {
+            const element = elementScroll
+                ? elementScroll
+                : component.parentNode;
+            element.addEventListener('scroll', onScroll);
+            element.addEventListener('resize', onScroll);
+        }
+    }
+
+    const onScroll = (e) => {
+        const scrollHeight = e.target.scrollHeight; // 브라우저의 스크롤 높이
+        const clientHeight = e.target.clientHeight; // 브라우저의 화면 높이
+        const scrollTop = e.target.scrollTop; // 브라우저의 현재 스크롤 위치
+        const realHeight = scrollHeight - clientHeight;
+        const triggerHeight = realHeight * 0.7;
+
+        const triggerComputed = () => {
+            return scrollTop > triggerHeight;
+        };
+
+        const scrollTrigger = () => {
+            return triggerComputed();
+        };
+
+        if (scrollTrigger()) {
+            currentArticlesPage.increPage();
+        }
+    };
+</script>
+
 <!-- start article-wrap -->
-<div class="articles-wrap" />
+<div class="articles-wrap" bind:this={component}>
+    {#each $articles.articleList as article, index}
+        <Article {article} />
+    {/each}
+</div>
 <!-- end article-wrap -->
